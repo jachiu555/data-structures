a80f4933b9355ccc0ba590cc67533986784ca827
James Chiu <jachiu555@gmail.com>
2019-09-05 16:17:40 -0700

    Completed stack in prototypal pattern. Added comment for better understanding.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
+// we want to create the stackMethods object each time this function is run, with the methods automatically applied to the object we make. no need for extend now
+
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.counter = 0;
+
+  return someInstance;
+
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.counter++;
+  this.storage[this.counter] = value;
+}
 
+stackMethods.pop = function() {
+  if (this.counter > 0) {
+    var prop = this.storage[this.counter];
+    delete this.storage[this.counter];
+    this.counter--;
+    return prop;
+  };
+};
+
+stackMethods.size = function() {
+  return this.counter;
+};
