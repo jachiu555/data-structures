fb909be7fe1307ab03189c9b241b31584e218da8
James Chiu <jachiu555@gmail.com>
2019-09-05 21:34:57 -0700

    Completed stack in the pseudoclassical pattern. Added comments for better understanding and readability.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,31 @@
+/*
+new keyword does object delegation for you, and it also returns an object
+
+pseudo-classical instantion allows a function to have methods, but will not need to return anything until the user or someone else explicitly declares a variable with the new keyword, which instantiates a new object, that object will have storage (which contains the standard object methods), counter, and a prototype object with push/pop/size as a nested object. a variable is assigned to new Stack();
+*/
+
+//this function doesn't return anything. removes the Object.create and return at the end
+
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};          // holds all the standard Object methods
+  this.counter = 0;
 };
 
+// create prototype object that has all these methods (push/pop/size) as well as the standard Object method that every object gets
+Stack.prototype.push = function(value) {
+  this.counter++
+  this.storage[this.counter] = value;
+};
 
+Stack.prototype.pop = function() {
+  if (this.counter > 0) {
+    var prop = this.storage[this.counter];
+    delete this.storage[this.counter];
+    this.counter--;
+    return prop;
+  };
+};
+
+Stack.prototype.size = function() {
+  return this.counter;
+};
