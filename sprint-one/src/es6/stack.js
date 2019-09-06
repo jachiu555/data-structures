ad5339114d8e1bea93c8e59fc932204ea39033d9
James Chiu <jachiu555@gmail.com>
2019-09-05 22:05:01 -0700

    Completed stack in the ES6 pattern.

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,24 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
-  }
+    this.storage = {};
+    this.counter = 0;
+  };
 
-}
+  push(value) {
+    this.counter++;
+    this.storage[this.counter] = value;
+  };
+
+  pop() {
+    if (this.counter > 0) {
+      var prop = this.storage[this.counter];
+      delete this.storage[this.counter];
+      this.counter--;
+      return prop;
+    };
+  };
+
+  size() {
+    return this.counter;
+  };
+};
