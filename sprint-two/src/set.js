bd2b7643cebc9600ba064a77029bf795606c8465
James Chiu <jachiu555@gmail.com>
2019-09-07 18:12:34 -0700

    Completed set.js. Added comments for better understanding and readability.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,21 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me                                // refer storage to an object
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = true;                                 // insert item into the storage object and set to true
 };
 
 setPrototype.contains = function(item) {
+  return this._storage[item];                                 // will return true if item is in storage object
 };
 
 setPrototype.remove = function(item) {
+  this._storage[item] = false;
 };
 
 /*
